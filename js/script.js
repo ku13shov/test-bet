77faa36bd1ca27e0b02de811f3b2831e4e24cad5
Пользователь <kuleshov000@gmail.com>
2022-02-08 09:14:48 +0300

    feat: add counter that shows spins count

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ const btnTurbo = document.querySelector('.panel__turbo-btn');
 const check = document.querySelector('.panel__turbo-check');
 const turboBg = document.querySelectorAll('.panel__turbo-bg');
 const btnAuto = document.querySelector('.panel__auto-btn');
+const autoCounter = document.querySelector('.panel__auto-counter');
 const btnSpin = document.querySelector('.panel__spin-btn');
 const spinSign = document.querySelector('.panel__spin-sign');
 const spins = document.querySelectorAll('.panel__auto-spin');
@@ -34,21 +35,111 @@ spins.forEach(item => {
 })
 
 //Auto list component---------------------------------------------
-btnAuto.addEventListener('mouseover', () => {
+const height195 = () => {
     select.style.height = '195px';
-});
+}
+
+const changeAutoBtn = () => {
+    btnAuto.textContent = 'STOP';
+    btnAuto.style.paddingLeft = '50px';
+    select.removeEventListener('mouseover', height195);
+    btnAuto.removeEventListener('mouseover', height195);
+    select.style.height = '0';
+}
+
+btnAuto.addEventListener('mouseover', height195);
 
 btnAuto.addEventListener('mouseout', () => {
     select.style.height = '0';
 });
 
-select.addEventListener('mouseover', () => {
-    select.style.height = '195px';
-});
+select.addEventListener('mouseover', height195);
 
 select.addEventListener('mouseout', () => {
     select.style.height = '0';
 });
+
+btnAuto.addEventListener('click', () => {
+    if (autoCounter.classList.contains('active-counter')) {
+        audioPlay();
+        autoCounter.classList.remove('active-counter');
+        height195();
+        btnAuto.addEventListener('mouseover', height195);
+        select.addEventListener('mouseover', height195);
+        btnAuto.textContent = 'AUTO';
+        btnAuto.style.paddingLeft = '35px';
+    }
+});
+
+spins.forEach(item => {
+    item.addEventListener('click', () => {
+        if (item.dataset.count <= 25) {
+            autoCounter.textContent = `${item.dataset.count - 1}`;
+            autoCounter.style.background = 'url(images/counter-blue.png) no-repeat center / contain';
+            autoCounter.classList.add('active-counter');
+
+            let count = item.dataset.count  - 2;
+            let countSignInterval = setInterval(() => {
+                autoCounter.textContent = `${count--}`;
+                if (count < 0) {
+                    clearInterval(countSignInterval);
+                    btnAuto.click();
+                }
+
+                btnAuto.addEventListener('click', () => {
+                    if (autoCounter.classList.contains('active-counter')) {
+                        audioPlay();
+                        autoCounter.classList.remove('active-counter');
+                        height195();
+                        btnAuto.addEventListener('mouseover', height195);
+                        select.addEventListener('mouseover', height195);
+                        btnAuto.textContent = 'AUTO';
+                        btnAuto.style.paddingLeft = '35px';
+                    }
+                    clearInterval(countSignInterval);
+                });
+            }, 1500);
+    
+            changeAutoBtn();
+        } else if (item.dataset.count > 25) {
+            autoCounter.style.background = 'url(images/counter-red.png) no-repeat center / contain';
+            autoCounter.textContent = '';
+            autoCounter.innerHTML = '<img class="panel__auto-counter-sign" src="images/counter-sign.png" alt="">';
+
+            const autoCounterSign = document.querySelector('.panel__auto-counter-sign');
+            let deg = 0;
+
+            setTimeout(() => {
+                deg -= 360;
+                autoCounterSign.style.transform = `rotate(${deg}deg)`;
+            }, 300);
+
+            let countSignInterval = setInterval(() => {
+                deg -= 360;
+                autoCounterSign.style.transform = `rotate(${deg}deg)`;
+                if (deg < -20000) {
+                    clearInterval(countSignInterval);
+                }
+
+                btnAuto.addEventListener('click', () => {
+                    if (autoCounter.classList.contains('active-counter')) {
+                        audioPlay();
+                        autoCounter.classList.remove('active-counter');
+                        height195();
+                        btnAuto.addEventListener('mouseover', height195);
+                        select.addEventListener('mouseover', height195);
+                        btnAuto.textContent = 'AUTO';
+                        btnAuto.style.paddingLeft = '35px';
+                    }
+                    clearInterval(countSignInterval);
+                });
+            }, 2300);
+            autoCounter.classList.add('active-counter');
+
+            changeAutoBtn();
+        }
+    });
+})
 
 //Total and coin inrc/decr-----------------------------------------
 totalBet.textContent = `¥${totalBetIncr}`;
@@ -242,23 +333,27 @@ btnTurbo.addEventListener('click', () => {
 });
 
 //Spin btn func----------------------------------------------
-const delPressed = () => {
+function deletePressed() {
     btnSpin.classList.remove('pressed');
     spinSign.style.animation = 'rotate 5.5s linear 2s infinite';
     spinSign.src = 'images/spin-sign.png';
     spinSign.style.width = '115px';
 }
 
+function addPressed() {
+    btnSpin.classList.add('pressed');
+    spinSign.style.animation = 'none';
+    spinSign.src = 'images/stop-normal.png';
+    spinSign.style.width = '60px';
+}
+
 btnSpin.addEventListener('click', () => {
-    let timeout = setTimeout(delPressed, 3000);
+    let timeout = setTimeout(deletePressed, 3000);
     
     if (btnSpin.classList.contains('pressed')) {
-        delPressed();
+        deletePressed();
         clearInterval(timeout);
     } else {
-        btnSpin.classList.add('pressed');
-        spinSign.style.animation = 'none';
-        spinSign.src = 'images/stop-normal.png';
-        spinSign.style.width = '60px';
+        addPressed();
     }
 });
